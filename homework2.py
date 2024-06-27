ac4204ba345053de5750ed0444de2b92acacaea1
xdvt <xdvt1337@gmail.com>
2024-06-27 07:03:29 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_assignments = 12
+spent_hours = 1.5
+course_name = "Python"
+time_per_assignment = (completed_assignments / spent_hours)
+print(f'Курс: {course_name}, всего задач: {completed_assignments}, затрачено часов:'
+      f' {spent_hours}, среднее время выполнения: {time_per_assignment} часов.')
