15e3ae0a5bcb83bfccc658395d181685922eff4d
xdvt <xdvt1337@gmail.com>
2024-06-28 08:42:12 +0700

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
 completed_assignments = 12
 spent_hours = 1.5
 course_name = "Python"
-time_per_assignment = (completed_assignments / spent_hours)
+time_per_assignment = (spent_hours / completed_assignments)
 print(f'Курс: {course_name}, всего задач: {completed_assignments}, затрачено часов:'
       f' {spent_hours}, среднее время выполнения: {time_per_assignment} часов.')
